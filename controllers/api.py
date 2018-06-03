c80ff1f3c772f35088a86f66d6637bbe649341ef
AJ Brown <chargers5583@gmail.com>
2018-06-03 12:48:18 -0700

    can successfully grab data from the database

--- controllers/api.py
+++ controllers/api.py
@@ -1,1 +1,17 @@
 # Here go your api methods.
+def get_posts():
+    dbPosts = db(db.volunteer_post).select()
+    posts = []
+
+    for i in dbPosts.sort(lambda i: i.updated_on):
+        t = dict(
+            id=i.id,
+            email=i.user_email,
+            title=i.title,
+            memo=i.memo,
+            time=i.updated_on,
+        )
+        posts.append(t)
+
+    return response.json(dict(posts=posts))
+
